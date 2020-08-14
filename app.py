65ecb308d420ff4b4e4ad9e4b1998bf419582bf3
jfto23 <jeanfrancoisto@hotmail.com>
2020-08-13 21:55:14 -0400

    working demo

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
 from flask import Flask
 from flask import render_template
 
-app = Flask(__name__)
+import os
 
-@app.after_request
-def add_header(response):
-    response.cache_control.max = 0
-    response.cache_control.public = True 
-    return response
+app = Flask(__name__)
 
 @app.route("/")
 def index():
